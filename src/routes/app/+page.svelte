9422b7b2d9e33914418ff2a9d4dd3e0eedf5db97
germangrigoriev <ggrigoriev.000spb@gmail.com>
2024-12-07 11:00:59 +0300

    chore: description

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -10,6 +10,13 @@
 
 		<Cta class="text-red" onclick={() => goto(routes.deck)}>Edit deck</Cta>
 		<Cta class="text-red" onclick={() => goto(routes.game)}>Start game</Cta>
+
+		<div class="flex justify-center p-10">
+			Шляпа (также шапка[1], кастрюлька[2]) — салонная командно-индивидуальная или парная
+			интеллектуальная игра, в которой игрок должен за небольшое время объяснить как можно больше
+			слов, чтобы его партнер их отгадал. Англоязычный аналог этой игры — «Alias» (об этой игре
+			написано ниже)[3].
+		</div>
 	</div>
 	<footer>I Am footer</footer>
 </div>
