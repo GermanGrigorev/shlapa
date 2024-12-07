0e6989d40a15dff0e487f2b669a11929749c62ec
germangrigoriev <ggrigoriev.000spb@gmail.com>
2024-12-07 11:24:21 +0300

    refactor: nav

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -1,16 +1,32 @@
 <script lang="ts">
-	import { Alert, DarkMode } from 'flowbite-svelte';
+	import { Alert, BottomNav, BottomNavItem, DarkMode } from 'flowbite-svelte';
 	import { routes } from '../routes';
+	import { HomeSolid, FolderOpenSolid, PlaySolid } from 'flowbite-svelte-icons';
+	import { page } from '$app/stores';
 	let { children } = $props();
+
+	let activeUrl = $derived($page.url.pathname);
 </script>
 
 <div class=" min-h-screen bg-amber-50 text-amber-950 dark:bg-amber-950 dark:text-amber-50">
-	<header>
-		<DarkMode />
-		<a href={routes.home}>Home</a>
-		<a href={routes.deck}>Deck</a>
-		<a href={routes.game}>Game</a>
-		<a href="/">Landing</a>
-	</header>
 	{@render children()}
+	<div>
+		<BottomNav {activeUrl} position="absolute" classInner="grid-cols-3">
+			<BottomNavItem href={routes.home} btnName="Home">
+				<HomeSolid
+					class="mb-1 h-6 w-6 text-gray-500 group-hover:text-primary-600 dark:text-gray-400 dark:group-hover:text-primary-500"
+				/>
+			</BottomNavItem>
+			<BottomNavItem href={routes.deck} btnName="Deck">
+				<FolderOpenSolid
+					class="mb-1 h-6 w-6 text-gray-500 group-hover:text-primary-600 dark:text-gray-400 dark:group-hover:text-primary-500"
+				/>
+			</BottomNavItem>
+			<BottomNavItem href={routes.game} btnName="Game">
+				<PlaySolid
+					class="mb-1 h-6 w-6 text-gray-500 group-hover:text-primary-600 dark:text-gray-400 dark:group-hover:text-primary-500"
+				/>
+			</BottomNavItem>
+		</BottomNav>
+	</div>
 </div>
